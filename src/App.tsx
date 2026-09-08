471649f7f234d710734b7758ac8a9d417fade904
Sai Sri Teja Unnava <sri.teja.st@gmail.com>
2026-09-08 17:38:44 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,8 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/skills" element={<Skills />} />
             <Route path="/projects" element={<Projects />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/case-studies" element={<CaseStudies />} />
+            {/* <Route path="/blog" element={<Blog />} /> */}
+            {/* <Route path="/case-studies" element={<CaseStudies />} /> */}
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
